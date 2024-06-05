24b58c5edd677cec59a926a8cd9940e9171513e3
ak513499786 <ak513499786@gmail.com>
2024-06-05 16:33:11 +0530

    Push

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+      <title>Parallel EDU</title>
       <Head />
       <body>
         <Main />
